e514524558e9a3344666c256befa340370c8db6f
floraExe <flora.kang0519@gmail.com>
2025-10-30 22:34:07 -0400

    we got a button!

--- app/(tabs)/promos.tsx
+++ app/(tabs)/promos.tsx
@@ -1,13 +1,40 @@
-import Header from "@/components/Header";
-import { View } from "react-native";
+import { StyleSheet } from "react-native";
+
 
 export default function FavoritesPage(){
 
 
-    return(
-        <View style={{ flex: 1 }}>
-            <Header userName="Kevin" welcomeMessage="Ravis de vous revoir !" />
-        </View>
-    )
+    return
+    
     
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+
+    button: {
+        flexDirection: 'row',
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        backgroundColor: "#7f3b00ff",
+        borderRadius: 20,
+        height: "15%",
+        width: "90%", 
+        alignItems: 'center',
+    },
+
+    circle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 65,
+        width: 65,
+        borderRadius: 70,
+        backgroundColor: "white",
+    },
+
+})
